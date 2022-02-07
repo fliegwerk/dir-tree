5c3891d590e9464cf40a0427b0b7e8f86367d511
Ludwig Richter <richter@fliegwerk.com>
2022-02-07 17:07:46 +0100

    chore: Fix build pipeline

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,25 @@
 import typescript from 'rollup-plugin-typescript2';
 import commonjs from 'rollup-plugin-commonjs';
 import * as pkg from './package.json';
-import sourcemaps from "rollup-plugin-sourcemaps";
+import sourcemaps from 'rollup-plugin-sourcemaps';
+import externals from 'rollup-plugin-node-externals';
 
 export default {
-    input: './src/index.ts',
+	input: './src/index.ts',
 
-    output: [
-        {
-            file: pkg.main,
-            format: 'cjs',
-            sourcemap: true
-        },
-        {
-            file: pkg.module,
-            format: 'es',
-            sourcemap: true
-        }
-    ],
+	output: [
+		{
+			file: pkg.main,
+			format: 'cjs',
+			sourcemap: true,
+			exports: 'default'
+		},
+		{
+			file: pkg.module,
+			format: 'es',
+			sourcemap: true
+		}
+	],
 
-    external: Object.keys(pkg['dependencies'] || {}).concat(
-        Object.keys(pkg["optionalDependencies"] || {})
-    ),
-
-    plugins: [typescript(), commonjs(), sourcemaps()]
+	plugins: [externals(), typescript(), commonjs(), sourcemaps()]
 };
