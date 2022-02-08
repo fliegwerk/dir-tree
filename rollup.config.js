281c901e870853c1bbc24933e5ed14e345e0d5fe
Ludwig Richter <richter@fliegwerk.com>
2022-02-08 22:18:35 +0100

    chore(deps): Move from `rollup-plugin-commonjs` to `@rollup/plugin-commonjs`

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import typescript from 'rollup-plugin-typescript2';
-import commonjs from 'rollup-plugin-commonjs';
+import commonjs from '@rollup/plugin-commonjs';
 import * as pkg from './package.json';
 import sourcemaps from 'rollup-plugin-sourcemaps';
 import externals from 'rollup-plugin-node-externals';
